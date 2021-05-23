c039b42e38a66733ce15e824d920815d8501479e
rubel islam <rubelislam301@gmail.com>
2021-05-23 13:01:33 +0600

    Add Models Folader and Two Model one is Employee and Other is Department

---
+++ DotNetWebApiReactJs/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public String EmployeeName { get; set; }
+        public String Department { get; set; }
+        public String DateOfJoining { get; set; }
+        public String PhotoFileName { get; set; }
+    }
+}
