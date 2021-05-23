c7b0b89a2ad35419debf7b39788f1690bae5693d
rubel islam <rubelislam301@gmail.com>
2021-05-24 00:06:33 +0600

    Add Database Script

---
+++ DotNetWebApiReactJs/DatabaseScript/DatabaseScript.sql
@@ -0,0 +1,18 @@
+﻿
+Create Database EmployeeDB;
+
+CREATE TABLE [dbo].[Department] (
+    [DepartmentId]   INT        IDENTITY (1, 1) NOT NULL,
+    [DepartmentName] NCHAR (10) NULL,
+    PRIMARY KEY CLUSTERED ([DepartmentId] ASC)
+);
+
+CREATE TABLE [dbo].[Employee] (
+    [EmployeeId]    INT           IDENTITY (1, 1) NOT NULL,
+    [EmployeeName]  VARCHAR (500) NULL,
+    [Department]    VARCHAR (500) NULL,
+    [DateOfJoining] DATE          NULL,
+    [PhotoFileName] VARCHAR (500) NULL,
+    PRIMARY KEY CLUSTERED ([EmployeeId] ASC)
+);
+
